dac039b2d7908da042dae5f4b06ffca26307ac7b
Shadow-nx <32244138+Shadow-nx@users.noreply.github.com>
2017-11-11 21:42:15 +0300

    Update main1.cpp

--- main1.cpp
+++ main1.cpp
@@ -1,41 +1,41 @@
 #include <iostream>
 #include <string>
+#include <cstring>
 #include <limits>
+#include <cstdlib>
 
 using namespace std;
 
-int main() {
-	int n=0;
-	int k=0;
-	int *z=NULL;
-	string str;
-	cin>>n;
-	cin.ignore(numeric_limits<streamsize>::max(), '\n');
-	getline(cin,str);
-	for(int i=0; i<str.length(); i++) {
-		if(str[i]>='0'&& str[i]<='9')
+int main()
+{
+string count;	
+string str1;
+int k=0;
+cin>>count;
+cin.ignore(numeric_limits<streamsize>::max(), '\n');
+getline(cin,str1);
+	for(int i=0; i<str1.length(); i++) {
+		if(str1[i]>='0'&& str1[i]<='9')
 			;
 		else k++;
 	}
 	k++;
-	if(n!=k) {
-		cout<<"An error has occurred while reading input data"<<endl;
-	} else {
-		z=new int[n];
-		for(int i=0; i<n; i++)
-			z[i]=0;
-		int y=0;
-		for(int i=0; i<str.length()+1; i++)
-			if(str[i]>='0'&&str[i]<='9')
-				z[y]=z[y]*10+str[i]-'0';
-			else y++;
+if(atoi(count.c_str())==k){
+string str2[k];
+for(int i=0,j=0;i<str1.length();i++)
+  if(str1[i]>='0' && str1[i]<='9')
+   str2[j]=str2[j]+str1[i];
+  else 
+    j++; 
+for(int i=0;i<k/2;i++)
+  swap(str2[i],str2[k-i-1]);
+str1="";
+for(int i=0;i<k;i++)
+  str1=str1+str2[i]+" ";
+  cout<<str1<<endl;
+}
+else
+ cerr<<"An error has occurred while reading input data"<<endl;
 
-		for(int i=0; i<n/2; i++)
-			swap(z[i],z[n-i-1]);
-
-		for(int i=0; i<n; i++)
-			cout<<z[i]<<" ";
-	}
-
-	return 0;
+return 0;	
 }
